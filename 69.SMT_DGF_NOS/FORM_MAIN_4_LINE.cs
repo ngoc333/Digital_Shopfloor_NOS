7db3d26cdf2ce95f54120d08b28f7af280f4aab7
phuocvj <lebaokhaky@gmail.com>
2020-10-02 13:51:52 +0700

    Phuoc them nut tms

--- 69.SMT_DGF_NOS/FORM_MAIN_4_LINE.cs
+++ 69.SMT_DGF_NOS/FORM_MAIN_4_LINE.cs
@@ -1154,7 +1154,7 @@ cmdQua1_Line2,cmdQua2_Line2,cmdQua3_Line2,cmdQua4_Line2,cmdQua5_Line2,cmdPro1_Li
             ComVar.Var._bValue1 = true;
             ComVar.Var.callForm = "1";
         }
-
+        
 
 
 
